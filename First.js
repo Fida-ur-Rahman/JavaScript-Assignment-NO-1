c31f93def1ea0d58f132af7ab5fbedde0e2edadf
Fida-ur-Rahman <fidasaba15@gmail.com>
2025-12-25 23:51:16 +0500

    JavaScript first Assignment

---
+++ First.js
@@ -0,0 +1,12 @@
+// My First Program
+console.log("Fida ur Rahman")
+
+// Alert 
+// alert("Fida ur Rahman")
+
+// Variable 
+// let 
+let name1="Fida ur Rahman Akhunzada";
+console.log(name1)
+let age = 25;
+console.log(age)
